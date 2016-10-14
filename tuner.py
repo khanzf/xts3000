2ad20377eae1ff794b6a9059fa86480eaeb81400
Farhan Khan <khanzf@gmail.com>
2016-10-14 00:33:12 -0400

    Trying to get radio and CPS communicating, still developmental.

--- tuner.py
+++ tuner.py
@@ -9,7 +9,15 @@ import xts
 def main():
     radio = xts.xts()
     radio.openserial('/dev/ttyUSB0')
+    print(radio.radioserial)
+    print("Sending initiate command")
+    print("The size is: %d" % len(xts.tuner_initialize))
     radio.sendecho(xts.tuner_initialize)
+    print("After initialize command")
+#    radio.sendsimple(xts.tuner_first)
+    radio._endlessRead()
+#    radio.sendsimple(xts.tuner_second)
+#    radio.sendsimple(xts.tuner_third)
 
 if __name__ == "__main__" :
     main()
--- xts.py
+++ xts.py
@@ -10,17 +10,26 @@ tuner_second =      b'\x01\x03\x00\x40\xEE\x01\x00\x1D\xB5'
 tuner_third =       b'\x00\x00\x12\x12\x01\x01\x06\x06\x02\x02\x50' # This is echoed back
 tuner_inforequest = b'\xF5\x11\x20\x00\x00\x00\xD9'
 
-class xts:
+class xts(object):
 
     def openserial(self, device):
-        self.radioserial = serial.Serial(device)
+        print("Reading: %s" % device)
+        self.radioserial = serial.Serial(device, timeout=1)
 
     def sendecho(self, cmd):
         for byte in cmd:
-            self.radioserial.write(bytes(byte))
+            self.radioserial.write(bytes([byte]))
             echobyte = self.radioserial.read() # Read one byte
+            print("Got this: %s" % bytes([echobyte]) )
             if echobyte is not byte:
                 raise Exception('The radio did not echo back the command')
 
     def sendsimple(self, cmd):
-        self.radioserial.write(cmd)
+        for byte in cmd:
+            self.radioserial.write(bytes([byte]))
+
+    def _endlessRead(self): # debug function
+        print("This is a debugging function and will not end")
+        while True:
+            tmpdata = self.radioserial.read()
+            print(ord(tmpdata))
